51cb36da05ccb0401a4dea0bdd54f436d9e88bf8
ernestLogitech <113042046+ernestLogitech@users.noreply.github.com>
2023-03-19 16:25:39 +0100

    19-03-2023

--- front/js/logincahe.js
+++ front/js/logincahe.js
@@ -1,5 +1,54 @@
+/*
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Sophie Bluel - Architecte d'intérieur</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Syne:wght@700;800&family=Work+Sans&display=swap" rel="stylesheet">
+	<meta name="description" content="">
+	<link rel="stylesheet" href="./assets/style.css">
+</head>
+<body>
+<header>
+	<h1>Sophie Bluel <span>Architecte d'inteérieur</span></h1>
+	<nav>
+		<ul> 
+			<li> <a class="projets" href="index.html">projets</a></li>
+			<li> <a class="contact" href="login.html">contact</a></li>
+            <li id="loginCon"> <a class="login" href="login.html">login</a></li> 
+			<li> <a class="Homepage" href="homepage_edit.html">Edit</a></li>
+			<li id="logout"> <a class="logout" onclick="deconnexion">logout</a></li>
+			<li><img src="./assets/icons/instagram.png" alt="Instagram"></li>
+		</ul>
+	</nav>
+</header>
+<body>
+    <section id="login">
+		<h2>Log In ${resultValues.userId}</h2>
+		<h2 id="nomemail"> '${stoLogin.email}'</h2>
+		<form action="#" method="post" id="form">
+			<label for="email">E-mail</label>
+			<input type="email" name="email" id="email">
+			<label for="name">Mot de passe</label>
+			<input type="password" name="password" id="password">
+			
+			<input type="submit" value="Se connecter">
+			<div id="res-error"></div>
+			<h2 class="mot-de-pass-oublier">Mot de passe oublié</h2>
+			
+		</form>
+	</section>
+	<script type="text/javascript" src="js/login.js"></script>
+</body>
+</html>
+*/ 
 
-    let resultValues;
+
+
+let resultValues;
     let login;
     let stoLogin;
     async function send(e) {
@@ -32,27 +81,18 @@
     getemailValidation();
     }
     else{
-      localStorage.setItem('login',login);
-     // stoLogin = localStorage.getItem('login');
-     stoLogin = login
-      location.href='index.html';
     result = JSON.stringify(result);
     login = JSON.stringify(login);
     localStorage.setItem('resultValue',result);
     localStorage.setItem('login',login);
     resultValues =localStorage.getItem('resultValue');
-    
+    stoLogin = localStorage.getItem('login');
     afficheruserconnect();
     resultValues =  JSON.parse(resultValues);
     console.log(resultValues);
     console.log(stoLogin);
-    /*setTimeout(() => {
-      document.location.reload();
-      location.href='index.html';
-    }, 3000);*/
-    
-    
-    //location.href='index.html';
+    //masquerLogin()
+    // location.href='index.html';
     }
 
     }
---
+++ front/js/testConnected.js
@@ -0,0 +1,29 @@
+let stoLogin = localStorage.getItem('login');
+if(stoLogin != null){
+afficherLogout();
+masquerLogin();
+}else{
+afficherLogin()
+masquerLogout()
+}
+function afficherLogout() {
+return document.getElementById("logout").style.display = "block";
+}
+function masquerLogout() {
+return document.getElementById("logout").style.display = "none";
+}
+function masquerLogin() {
+return document.getElementById("loginCon").style.display ="none";
+}
+function afficherLogin() {
+return document.getElementById("loginCon").style.display ="block";
+}
+document.getElementById("logout").addEventListener("click", function() {   
+    localStorage.clear();
+    //sessionStorage
+    //stoLogin = localStorage.setItem("stoLogin", "[]");
+    //stoLogin = window.localStorage.removeItem("stoLogin","[]");
+  // window.localStorage.removeItem("stoLogin"); faudrai ecrsser le login
+console.log(stoLogin)
+location.href='login.html';
+});
