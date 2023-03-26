4c304af82b8044199474a2e4643902bb5a736216
ernestLogitech <113042046+ernestLogitech@users.noreply.github.com>
2023-03-26 06:10:29 +0100

    26-03-2023

--- front/js/homepage_edit.js
+++ front/js/homepage_edit.js
@@ -1,127 +1,167 @@
-const reponse = await fetch('http://localhost:5678/api/works',{
-
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Accept": "application/json", 
-      "Content-Type":"application/json",
-      "Cross-Origin-Resource-Policy" : "same-site | same-origin | cross-origin",
-    }
-  });
+const reponse = await fetch("http://localhost:5678/api/works", {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Cross-Origin-Resource-Policy": "same-site | same-origin | cross-origin",
+  },
+});
 const works = await reponse.json();
 console.log(works);
 function genererGallery(works) {
-for(let i = 0; i < works.length ; i++){
+  for (let i = 0; i < works.length; i++) {
     const galery = works[i];
     const figureElement = document.createElement("figure");
     const imageElement = document.createElement("img");
     const imageElementdiv = document.createElement("div");
     const imageElementfavdelete = document.createElement("i");
-    imageElement.src = galery.imageUrl; 
-    const titreFigure = document.createElement("figcaption"); 
-    titreFigure.innerText = 'éditer' 
+    imageElement.src = galery.imageUrl;
+    const titreFigure = document.createElement("figcaption");
+    titreFigure.innerText = "éditer";
     const divisionGallery = document.querySelector(".gallerymodal");
 
     divisionGallery.appendChild(figureElement);
-    figureElement.appendChild(imageElement); 
-    imageElementdiv.className = 'deleteBlack';
+    figureElement.className = "fig";
+    figureElement.setAttribute("id", galery.id);
+    figureElement.classList.add("fig");
+    figureElement.appendChild(imageElement);
+    imageElementdiv.className = "deleteBlack";
     imageElementdiv.classList.add("deleteBlack");
-    figureElement.appendChild(imageElementdiv); 
+    figureElement.appendChild(imageElementdiv);
     figureElement.appendChild(titreFigure);
     titreFigure.classList.add("nopad");
     figureElement.appendChild(imageElementfavdelete);
     imageElementfavdelete.classList.add("fa-regular");
     imageElementfavdelete.classList.add("fa-trash-can");
     imageElementfavdelete.classList.add("modefav");
-}
-    
+  }
 }
 genererGallery(works);
 
+function genererGalleryHomeEdit(works) {
+  for (let i = 0; i < works.length; i++) {
+    const galleryHomeEdit = works[i];
+    const figureElement = document.createElement("figure");
+    const imageElement = document.createElement("img");
+    imageElement.src = galleryHomeEdit.imageUrl;
+    const titreFigure = document.createElement("figcaption");
+    titreFigure.innerText = galleryHomeEdit.title;
+    const sectionPortfolio = document.querySelector("#portfolio");
+    const divisionGallery = document.querySelector(".galleryHomeEdit");
+    sectionPortfolio.appendChild(divisionGallery);
+    divisionGallery.appendChild(figureElement);
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(titreFigure);
+  }
+}
+genererGalleryHomeEdit(works);
 
-  async function send_modal(e) {
+async function send_modal(e) {
+  e.preventDefault();
+  console.log("bien");
+  let connectToken = JSON.parse(localStorage.getItem("connectToken"));
+  let title = document.getElementById("name-mod").value;
+  let category = document.getElementById("cat-select").value;
+  let imageTosend = document.getElementById("file-ajouter-photo").files[0];
+  let userId = connectToken.userId;
+  let token = connectToken.token;
+  const formData = new FormData();
 
-    e.preventDefault();
-    console.log('bien');
-    let connectToken =JSON.parse(localStorage.getItem('connectToken')) ;
-    var title = document.getElementById("name-mod").value; 
-    var category = document.getElementById("cat-select").value;
-   // console.log(document.querySelector('input[type="file"]').files);
-    const sendFiles = document.querySelector('input[type="file"]').files
-    ///const fileInput = document.querySelector('input[type=file]');
-    console.log(sendFiles[0]);
-    const formData = new FormData();
-  
-
-    //formData.append('file', sendFiles[0]);
-   const url = formData.append('file',sendFiles[0])
-   var userId = connectToken.userId;
-    var token = connectToken.token;
-    //var userId = connectToken;
-    formData.append('title', title);
-    formData.append('category',category);
-    formData.append('userId',userId);
-    console.log(title);
-    console.log(category);
-    console.log(userId);
-    console.log(formData);
-    //console.log(connectToken.token);
-    console.log("je suis dans la function modal");
-    let response;
-    //let auth;
-    
-    if((title !="") && (category !="")){
-    let data_modal = { 
-      url:url,
-      title:title,
-      category:category,
-      userId:userId,
-      //auth : {userId:userId}
-    }
-
-    response = await fetch('http://localhost:5678/api/works', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json", 
-    //'Content-Type': 'multipart/form-data',
-    'authorization': 'Bearer '+ token
-    },
-    body:  JSON.stringify(data_modal) ,
-    //headers: {Authentication: `Bearer ${token}`}
-    //headers: {Authentication: 'Bearer {token}'}
-
-    }).then(response => response.json())
-    .then(data =>{ console.log(data)
-    
+  formData.append("title", title);
+  formData.append("category", category);
+  formData.append("userId", userId);
+  formData.append("image", imageTosend);
+  console.log(title);
+  console.log(category);
+  console.log(userId);
+  console.log(formData);
+  //console.log(connectToken.token);
+  console.log("je suis dans la function modal");
+  let response;
+  //let auth;
+  if (title != "" && category != "") {
+    response = await fetch("http://localhost:5678/api/works", {
+      method: "POST",
+      headers: {
+        //  "Content-Type": "application/json",
+        //'Content-Type': 'multipart/form-data',
+        authorization: "Bearer " + token,
+      },
+      body: formData,
+      //headers: {Authentication: `Bearer ${token}`}
+      //headers: {Authentication: 'Bearer {token}'}
     })
-    .catch(function(error) {
-      console.log('Il y a eu un problème avec l\'opération fetch : ' + error.message);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(function (error) {
+        console.log(
+          "Il y a eu un problème avec l'opération fetch : " + error.message
+        );
+      });
+  } else {
+    getemailValidationVide();
+    console.log("vous devez renseigner vos informations de connexion");
+  }
+}
 
-  }else{
-    getemailValidationVide()
-    console.log('vous devez renseigner vos informations de connexion');
-  }  
- 
- }
-
-
-
-
-
-
- document.getElementById("form-modal2").addEventListener("submit", function(e) {
+document.getElementById("form-modal2").addEventListener("submit", function (e) {
   send_modal(e);
-  });
+});
 
-  document.getElementById("name-mod").addEventListener("input", function(e) {
-    document.getElementById("for-name").innerText = e.target.value;
-    });
-    
-document.getElementById("cat-select").addEventListener("change", function(e) { 
+document.getElementById("name-mod").addEventListener("input", function (e) {
+  document.getElementById("for-name").innerText = e.target.value;
+});
+
+document.getElementById("cat-select").addEventListener("change", function (e) {
   document.getElementById("for-select").innerText = e.target.value;
-  });
+});
 
- /*
+// suppression des elements du tableau
+const modalGallery = Array.from(document.querySelectorAll(".fig"));
+let imageEncours;
+
+console.log(modalGallery);
+let indexEncours;
+modalGallery.forEach((element) => {
+  element.addEventListener("click", async (e) => {
+    console.log(works);
+    imageEncours = e.target;
+    console.log(modalGallery);
+    console.log(element);
+    indexEncours = modalGallery.indexOf(element);
+    console.log(indexEncours);
+
+    let connectToken = JSON.parse(localStorage.getItem("connectToken"));
+    let token = connectToken.token;
+    let id;
+    id = element.getAttribute("id");
+    console.log(id);
+    response = await fetch("http://localhost:5678/api/works/" + id, {
+      method: "DELETE",
+      headers: {
+        //  "Content-Type": "application/json",
+        //'Content-Type': 'multipart/form-data',
+        authorization: "Bearer " + token,
+      },
+
+      //headers: {Authentication: `Bearer ${token}`}
+      //headers: {Authentication: 'Bearer {token}'}
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(function (error) {
+        console.log(
+          "Il y a eu un problème avec l'opération fetch : " + error.message
+        );
+      });
+  });
+});
+
+/*
   const image_send = document.querySelector("#file-ajouter-photo");
 var upload_image = "";
 image_send.addEventListener("change", function(){
@@ -136,9 +176,6 @@ image_send.addEventListener("change", function(){
   });
   */
 
-
-
-   
 /*
 const im = document.getElementById("file-ajouter-photo");
 im.addEventListener("input", function(e) {
@@ -152,9 +189,7 @@ im.addEventListener("input", function(e) {
   document.getElementById("image-mod").appendChild(imagePhoto);
   });*/
 
-
-
-  /*
+/*
     async function send_modal(e) {
 
     e.preventDefault();
--- front/js/login.js
+++ front/js/login.js
@@ -1,111 +1,82 @@
+let resultValues;
+let login;
+let stoLogin;
+let connectToken;
+let result;
+async function send(e) {
+  e.preventDefault();
+  var vemail = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  console.log(vemail);
+  console.log(password);
+  console.log("je suis dans la function");
+  let response;
+  if (vemail != "" && password != "") {
+    login = {
+      email: vemail,
+      password: password,
+    };
 
-    let resultValues;
-    let login;
-    let stoLogin;
-    let connectToken;
-    let result ;
-    async function send(e) {
-    e.preventDefault();
-    var vemail = document.getElementById("email").value; 
-    var password = document.getElementById("password").value;   
-    console.log(vemail);
-    console.log(password);
-    console.log("je suis dans la function");
-    let response;
-    if((vemail !="") && (password !="")){
-    login = { 
-    email:vemail,
-    password:password
-    }
-
-    response = await fetch('http://localhost:5678/api/users/login', {
-    method: 'POST',
-    headers: {
-    'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(login)
-    }).then(response => response.json())
-    .then(data =>{ console.log(data)
-    
-     console.log(result)
-      if(data.token) { 
-        console.log('connexion reussit');
-        login = JSON.stringify(login);
-        result = JSON.stringify(data)
-        localStorage.setItem('connectLogin',login);
-        localStorage.setItem('connectToken',result);
-        stoLogin = localStorage.getItem('connectLogin');
-        connectToken = localStorage.getItem('connectToken');
-        console.log(connectToken)
-        console.log(stoLogin)
-        setTimeout(() => {
-          //document.location.reload();
-          location.href='index.html';
-        }, 10000); 
-      }else{
-        getemailValidation()
-        console.log(data.error)
-      }
+    response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(login),
     })
-    .catch(function(error) {
-      console.log('Il y a eu un problème avec l\'opération fetch : ' + error.message);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
 
-  }else{
-    getemailValidationVide()
-    console.log('vous devez renseigner vos informations de connexion');
-  }  
- }
-    /*
-    let result = await response.json();
-    // console.log(result.error);
-    if(result.error){
-    getemailValidation();
-    }
-    else{
-      localStorage.setItem('login',login);
- 
-    localStorage.setItem('resultValue',result);
-     // stoLogin = localStorage.getItem('login');
-     stoLogin = login
-    result = JSON.stringify(result);
-    login = JSON.stringify(login);
-    localStorage.setItem('login',login);
-    resultValues =localStorage.getItem('resultValue');
-    
-    afficheruserconnect();
-    resultValues =  JSON.parse(resultValues);
-    console.log(resultValues); 
-    console.log(stoLogin);
-    
-    }
+        console.log(result);
+        if (data.token) {
+          console.log("connexion reussit");
+          login = JSON.stringify(login);
+          result = JSON.stringify(data);
+          localStorage.setItem("connectLogin", login);
+          localStorage.setItem("connectToken", result);
+          stoLogin = localStorage.getItem("connectLogin");
+          connectToken = localStorage.getItem("connectToken");
+          console.log(connectToken);
+          console.log(stoLogin);
+          setTimeout(() => {
+            location.href = "index.html";
+          }, 10000);
+        } else {
+          getemailValidation();
+          console.log(data.error);
+        }
+      })
+      .catch(function (error) {
+        console.log(
+          "Il y a eu un problème avec l'opération fetch : " + error.message
+        );
+      });
+  } else {
+    getemailValidationVide();
+    console.log("vous devez renseigner vos informations de connexion");
+  }
+}
 
- 
-    catch(error){
-    //  console.log(error);
-    getemailValidation();
-    }
+document.getElementById("form").addEventListener("submit", send);
 
+document.getElementById("email").addEventListener("input", function (e) {
+  document.getElementById("res-error").style.display = "none";
+});
+document.getElementById("password").addEventListener("input", function (e) {
+  document.getElementById("res-error").style.display = "none";
+});
+function getemailValidation() {
+  document.getElementById("res-error").style.display = "block";
+  document.getElementById("res-error").innerText =
+    "Email ou Mot de passe invalide";
+}
+function getemailValidationVide() {
+  document.getElementById("res-error").style.display = "block";
+  document.getElementById("res-error").innerText =
+    "Renseigner vos informations de connexion svp !";
+}
 
-    }*/
-    
-    document.getElementById("form").addEventListener("submit", send);
-
-    document.getElementById("email").addEventListener("input", function(e) {
-    document.getElementById("res-error").style.display = "none";    
-    });
-    document.getElementById("password").addEventListener("input", function(e) {
-      document.getElementById("res-error").style.display = "none";    
-    });
-    function getemailValidation() {
-      document.getElementById("res-error").style.display = "block"; 
-      document.getElementById("res-error").innerText = "Email ou Mot de passe invalide";
-    }
-    function getemailValidationVide() {
-      document.getElementById("res-error").style.display = "block"; 
-        document.getElementById("res-error").innerText = "Renseigner vos informations de connexion svp !";
-      }
-
-    function afficheruserconnect() {
-    return document.getElementById("nomemail").innerText = login.email;
-    }
+function afficheruserconnect() {
+  return (document.getElementById("nomemail").innerText = login.email);
+}
